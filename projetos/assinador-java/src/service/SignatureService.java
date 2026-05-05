8ca026a08acf5b3d0ecf4c700ed49b70042425e3
Brenner Rodrigues <166179540+Brennerodrigues@users.noreply.github.com>
2026-05-05 20:25:48 -0300

    Create SignatureService interface for signing and validation

---
+++ projetos/assinador-java/src/service/SignatureService.java
@@ -0,0 +1,10 @@
+package br.gov.go.ses.assinador.service;
+
+import br.gov.go.ses.assinador.model.AssinadorResponse;
+import br.gov.go.ses.assinador.model.SignRequest;
+import br.gov.go.ses.assinador.model.ValidateRequest;
+
+public interface SignatureService {
+    AssinadorResponse sign(SignRequest request);
+    AssinadorResponse validate(ValidateRequest request);
+}
